c86812346b8b3e0ec47d0b185d255cd89d6df2d5
Shrilakshmisr <159100816+Shrilakshmisr@users.noreply.github.com>
2025-07-16 13:27:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const app = express();
-const studentRoutes = require('./routes/studentRoutes'); // Correct path
+const express=require('express')
+const db=require('./db-connection');
+const studentRoutes=require('../routes/studentsRoutes');
+
+//models
+const StudentModel=require('../models/students');
+
+const app=express();
 
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-  res.send('Hello world');
+    res.send('Hello world');
 })
-app.use('/students', studentRoutes); // Route setup
 
-app.listen(3000, (err) => {
-  console.log('Server started at http://localhost:3000');
+app.use("/students", studentRoutes);
+
+db.sync({force:true}).then((err)=>{
+    app.listen(3000,(err)=>{
+    console.log("Server is running");
+    })
+
+}).catch((err)=>{
+    console.log(err);
 })
+
